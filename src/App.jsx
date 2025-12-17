d4cb4d1d49ec24cb93a2fb165bc4fd3d42cf7881
amardom27 <amardom027@g.educaand.es>
2025-12-17 17:09:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
   
   return (
-    <>
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
